775083309d3500f4813e830695c3d9d3a165bf1e
Allan Gachomo <allangachomo37@gmail.com>
2025-06-24 17:25:39 +0300

    Vercel Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 import ErrorBoundary from './components/ErrorBoundary';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 createRoot(rootElement).render(
   <StrictMode>
+    <SpeedInsights />
     <ErrorBoundary>
       <Router>
         <App />

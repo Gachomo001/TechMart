63520f06f415773113ce0da416f9ec5019421e83
Allan Gachomo <allangachomo37@gmail.com>
2025-09-22 19:45:19 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), '');
   
